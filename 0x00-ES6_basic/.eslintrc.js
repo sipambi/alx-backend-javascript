dba8c0b55d4f7a06161885793e891ad6e860cc75
sipambi <sibandahmax@gmail.com>
2022-12-20 11:45:00 +0200

    updated config files

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,24 +1,36 @@
+
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: false,
+        es6: true,
+        jest: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+    extends: [
+        'airbnb-base',
+        'plugin:jest/all',
     ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: ['jest'],
+    rules: {
+        'no-console': 'off',
+        'no-shadow': 'off',
+        'no-restricted-syntax': [
+            'error',
+            'LabeledStatement',
+            'WithStatement',
+        ],
+    },
+    overrides: [
+        {
+            files: ['*.js'],
+            excludedFiles: 'babel.config.js',
+        }
+    ]
+};
